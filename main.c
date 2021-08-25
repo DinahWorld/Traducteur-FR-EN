512dde8972ec25e7c60c60eb58b6344c29e02f19
Dinath <dinath@pop-os.localdomain>
2021-08-25 22:31:39 +0200

    Amélioration du code et changement d'api

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 #include <string.h>
 
+#include<stdlib.h>
+
 
 struct MemoryStruct {
     char * memory;
@@ -31,12 +33,11 @@ WriteMemoryCallback(void * contents, size_t size, size_t nmemb, void * userp) {
     return realsize;
 }
 
+/* Show translation only */
 void printTrad(char * str) {
-    for (int i = 44; i < strlen(str); i++) {
-
+    for (int i = 46; i < strlen(str); i++) {
         if (str[i] != '"')
             printf("%c", str[i]);
-
         else {
             printf("\n");
             break;
@@ -45,36 +46,47 @@ void printTrad(char * str) {
     }
 }
 
-int main() {
-    FILE* file = NULL;
-    char key[51];
-    char keyArgum[67];
-    struct MemoryStruct chunk;
-
-    chunk.memory = malloc(1); /* will be grown as needed by the realloc above */
-    chunk.size = 0; /* no data at this point */
+/* Main Program */
+void curlTrad(int lang){
 
     char wanted[100];
     char * trad = NULL;
     int sizeOfWanted = 0;
 
-    file = fopen("key.txt", "r+");
-    fgets(key,51,file);
+    /* Your Api Key */
+    char key[] = "Key";
+
+    struct MemoryStruct chunk;
+
+    /* will be grown as needed by the realloc above */
+    chunk.memory = malloc(1);
+    /* no data at this point */ 
+    chunk.size = 0; 
     
-    if(file != NULL){
-      fgets(key,sizeof key,file);
-      sprintf(keyArgum, "x-rapidapi-key: %s", key);
+
+    /*We get the sentence we want to translate*/
+    fgets(wanted, 100, stdin);
+
+    /*remove "\n" from the sentence*/
+    sizeOfWanted = strcspn(wanted, "\n");
+    wanted[sizeOfWanted] = 0; 
+
+    /*the string that we are going to add will be an array of chars of 33 elements*/
+    trad = malloc((sizeOfWanted + 33) * sizeof(char));
+
+    /* Complete the sentence to be sent */
+    if(lang == 0){
+        sprintf(trad, "{\"text\":[\"%s\"],\"model_id\":\"fr-en\"}", wanted);
+    }else{
+        sprintf(trad, "{\"text\":[\"%s\"],\"model_id\":\"en-fr\"}", wanted);
     }
-    else{
-      printf("Error no Key\n");
-      exit(-1);
-    }
-    fclose(file);
-    
+
     CURL * hnd = curl_easy_init();
 
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_easy_setopt(hnd, CURLOPT_URL, "https://google-translate1.p.rapidapi.com/language/translate/v2");
+    curl_easy_setopt(hnd, CURLOPT_URL, "https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/3bfff264-49fc-45f1-b77e-f652c0237310/v3/translate?version=2018-05-01");
+    curl_easy_setopt(hnd, CURLOPT_USERPWD, "apikey");
+    curl_easy_setopt(hnd, CURLOPT_PASSWORD, key);
 
     curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
@@ -82,24 +94,12 @@ int main() {
     curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void * ) & chunk);
 
     struct curl_slist * headers = NULL;
-    headers = curl_slist_append(headers, "content-type: application/x-www-form-urlencoded");
-    headers = curl_slist_append(headers, "accept-encoding: application/gzip");
-    headers = curl_slist_append(headers, "x-rapidapi-host: google-translate1.p.rapidapi.com");
-    headers = curl_slist_append(headers, keyArgum);
+    headers = curl_slist_append(headers, "Content-Type: application/json");
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
-
-    //Need to comment
-    fgets(wanted, 100, stdin);
-
-    //remove "\n" from the text
-    sizeOfWanted = strcspn(wanted, "\n");
-    wanted[sizeOfWanted] = 0;
-
-    //le string que l'on va ajouter sera un tableau de char de 23 éléments
-    trad = malloc((sizeOfWanted + 23) * sizeof(char));
-    sprintf(trad, "q=%s&target=en&source=fr", wanted);
-
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0L);
     curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, trad);
+    
     CURLcode ret = curl_easy_perform(hnd);
 
     if (ret != CURLE_OK) {
@@ -114,6 +114,21 @@ int main() {
 
     free(chunk.memory);
     free(trad);
+}
+int main(int argc, char *argv[] ) {
+    /*The default language is French, if the user wants to translate from English */
+    /* he need add "en" */
+    if(argc == 2)
+    {  
+        if(strcmp(argv[1],"en") == 0)
+            curlTrad(1);
+        else
+            curlTrad(0);
+    }
+    else{
+        curlTrad(0);
+    }
+        
 
     return 0;
 
